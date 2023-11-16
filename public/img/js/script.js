e6c0467374d8e34f1c2a35bb628e7faa834695b8
gabriela moreno <gabriela.a.moreno@gmail.com>
2023-11-16 09:38:15 -0300

    se hacen cambios de carpetas

--- public/img/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const filterIcon = document.getElementById("filter-icon");
-    const filterDropdown = document.getElementById("filter-dropdown");
-
-    filterIcon.addEventListener("click", function() {
-        filterDropdown.classList.toggle("open");
-    });
-});
