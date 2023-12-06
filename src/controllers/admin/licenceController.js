b4822739cecb72c016b02fac679507ac18707a9b
gabriela moreno <gabriela.a.moreno@gmail.com>
2023-12-06 12:28:24 -0300

    .

--- src/controllers/admin/licenceController.js
+++ src/controllers/admin/licenceController.js
@@ -30,12 +30,10 @@ const store = async (req, res) => {
 	}
   
 	try {
-		console.log(req.body);
 	  const licence = await model.create(req.body);
 	  
 	  res.redirect("/licence");
 	} catch (error) {
-		console.log(error);
 	  res.status(500).send("Error al crear la licencia");
 	}
   };
