23328d25b0c078d68e735ce3cf7447cda444c1b1
gabriela moreno <gabriela.a.moreno@gmail.com>
2023-12-06 15:21:03 -0300

    Se agrega logica para sumar cantidades en cartItem

--- src/controllers/shopController.js
+++ src/controllers/shopController.js
@@ -64,14 +64,12 @@ const addToCart = async (req, res) => {
     try {
       const userId = req.user.id;
       const productId = req.params.id;
-      const quantity = req.body.quantity || 1; // Si no se proporciona una cantidad, usa 1 por defecto
+      const quantity = req.body.quantity || 1;
   
-      // Verificar si el usuario tiene un carrito activo
       let cart = await Cart.findOne({
         where: { UserId: userId, status: 'active' }
       });
   
-      // Si no tiene un carrito activo, crea uno
       if (!cart) {
         cart = await Cart.create({
           UserId: userId,
@@ -79,24 +77,16 @@ const addToCart = async (req, res) => {
         });
       }
   
-      // Verificar si el producto ya está en el carrito
-      const existingCartItem = await CartItem.findOne({
-        where: { CartId: cart.id, ProductId: productId }
+      const [cartItem, created] = await CartItem.findOrCreate({
+        where: { CartId: cart.id, ProductId: productId },
+        defaults: { Quantity: quantity }
       });
-  
-      // Si ya existe, simplemente actualiza la cantidad
-      if (existingCartItem) {
-        existingCartItem.quantity += parseInt(quantity, 10);
-        await existingCartItem.save();
-      } else {
-        // Si no existe, crea un nuevo elemento de carrito
-        await CartItem.create({
-          CartId: cart.id,
-          ProductId: productId,
-          Quantity: quantity
-        });
-      }
-  
+
+      if (!created) {
+      cartItem.Quantity += parseInt(quantity, 10);
+      await cartItem.save();
+    }
+
       res.redirect('/shop/cart');
     } catch (error) {
       console.error(error);
