7dd1e8fdf63c7d505ebd5bb4dfa8fc2ac65566b4
gabriela moreno <gabriela.a.moreno@gmail.com>
2023-12-05 21:58:38 -0300

    se suben validaciones

--- src/routes/admin/productRoutes.js
+++ src/routes/admin/productRoutes.js
@@ -5,7 +5,26 @@ const upload = multer({storage: multer.memoryStorage()});
 
 const { body } = require("express-validator");
 
+const isNumericOrEmpty = (value) => {
+    if (value === undefined || value === null || value === '') {
+      return true;
+    }
+  
+    const numericValue = parseFloat(value);
+
+    if (isNaN(numericValue)) {
+        return false;
+      }
+  
+      return true;
+  };
+
+  
 const validations = [
+    body("CategoryId")
+    .not()
+    .isEmpty()
+    .withMessage("La categoría es requerida"),
     body("nombre")
     .not()
     .isEmpty()
@@ -16,7 +35,7 @@ const validations = [
     body("codigo")
     .not()
     .isEmpty()
-    .withMessage("El SKU es requerido")
+    .withMessage("El código es requerido")
     .isLength({min:3})
     .withMessage("El código debe tener por lo menos 3 caracteres"),
     body("precio")
@@ -28,16 +47,10 @@ const validations = [
     .isLength({min:1})
     .withMessage("El precio debe tener por lo menos 1 dígito"),
     body("descuento")
-    .isNumeric()
+    .custom(isNumericOrEmpty)
     .withMessage("El descuento debe ser numérico")
     .isFloat({ min: 0, max: 100 })
     .withMessage("El descuento debe estar en el rango de 0 a 100"),
-    body("CategoryId")
-    .not()
-    .isEmpty()
-    .withMessage("La categoría es requerida")
-    .isLength({min:3})
-    .withMessage("La categoría debe tener por lo menos 3 caracteres"),
 ];
 
 const controller = require('../../controllers/admin/productController');
@@ -45,10 +58,10 @@ const controller = require('../../controllers/admin/productController');
 router.get('/', controller.index);
 
 router.get('/create', controller.create);
-router.post('/create', upload.single("imagen"), controller.store);
+router.post('/create', upload.single("imagen"), validations, controller.store);
 
 router.get('/edit/:id', controller.edit);
-router.put('/edit/:id', upload.single("imagen"), controller.update);
+router.put('/edit/:id', upload.single("imagen"), validations, controller.update);
 
 router.delete('/:id', controller.destroy);
 
